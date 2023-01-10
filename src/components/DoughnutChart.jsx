4c7f6a982765268a0eceb37aa0bb4c93d67e7036
TowmirAtInsta <towmir@instawebworks.com.au>
2023-01-10 14:06:13 +0600

    added chnages

--- src/components/DoughnutChart.jsx
+++ src/components/DoughnutChart.jsx
@@ -1,6 +1,11 @@
 import { Box } from '@mui/material'
 import React from 'react'
 
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
 const DoughnutChart = () => {
   return (
     <Box
@@ -9,7 +14,21 @@ const DoughnutChart = () => {
             height: "300px",
             backgroundColor: "#d9d9d9"
         }}
-    >DoughnutChart</Box>
+    >
+      <Box
+            sx={{
+                width: "100%",
+                height: "100%",
+                display: "flex",
+                // flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "center",
+                pb: "0.5rem"
+            }}
+        >
+            <Pie data={data} />
+        </Box>
+    </Box>
   )
 }
 
---
+++ src/mockDonutData.js
@@ -0,0 +1,22 @@
+const data = {
+    labels: ['Axial', 'Email Campaign', 'Warm Rolodex', 'OPUX', 'Introduction'],
+    datasets: [
+      {
+        label: '# of Completions',
+        data: [4, 2, 2, 3, 2],
+        backgroundColor: [
+          '#6aa84f',
+          '#4a86e8',
+          '#ff0000',
+        ],
+        borderColor: [
+          '#6aa84f',
+          '#4a86e8',
+          '#ff0000',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  }
+  
+  export default data;
