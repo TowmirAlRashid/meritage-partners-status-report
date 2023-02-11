db30d6bdb25c04b4470bff30a0aa62ba144c4c9d
AL RASHID TOWMIR <117705258+TowmirAtInsta@users.noreply.github.com>
2023-02-11 10:15:24 +0600

    added changes

--- src/components/DoughnutChart.jsx
+++ src/components/DoughnutChart.jsx
@@ -1,35 +1,37 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import { Box } from "@mui/material";
+import React from "react";
 
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { Pie } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+
+import data from "../mockDonutData";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const DoughnutChart = () => {
   return (
     <Box
-        sx={{
-            width: "45%",
-            height: "300px",
-            backgroundColor: "#d9d9d9"
-        }}
+      sx={{
+        width: "45%",
+        height: "300px",
+        backgroundColor: "#d9d9d9",
+      }}
     >
       <Box
-            sx={{
-                width: "100%",
-                height: "100%",
-                display: "flex",
-                // flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "center",
-                pb: "0.5rem"
-            }}
-        >
-            <Pie data={data} />
-        </Box>
+        sx={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          // flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          pb: "0.5rem",
+        }}
+      >
+        <Pie data={data} />
+      </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default DoughnutChart
+export default DoughnutChart;
