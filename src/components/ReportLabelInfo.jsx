ad50dd306d1296c81113124f33a496bcf36489d1
AL RASHID TOWMIR <117705258+TowmirAtInsta@users.noreply.github.com>
2023-02-21 06:40:35 +0600

    changed the logo

--- src/components/ReportLabelInfo.jsx
+++ src/components/ReportLabelInfo.jsx
@@ -1,7 +1,7 @@
 import { Box, Typography } from "@mui/material";
 import React from "react";
 
-import Logo from "../assets/Logo.png";
+import Logo from "../assets/Logo.jpeg";
 
 const ReportLabelInfo = ({
   engagementResponse,
